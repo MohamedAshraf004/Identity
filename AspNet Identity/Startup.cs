b9c424766f7cbc402ca0720d5e6dd4fa99e35ba1
MohamedAshraf004 <52258427+MohamedAshraf004@users.noreply.github.com>
2019-06-27 09:50:54 +0200

    Identity

---
+++ AspNet Identity/Startup.cs
@@ -0,0 +1,59 @@
+﻿using AspNet_Identity.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNet_Identity.Startup))]
+namespace AspNet_Identity
+{
+    public partial class Startup
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRoles();
+            CreateUser();
+        }
+
+        public void CreateRoles()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            IdentityRole role;
+            if(!roleManager.RoleExists("Admins"))
+            {
+                role = new IdentityRole();
+                role.Name = "Admins";
+                roleManager.Create(role);
+            }
+
+            if(!roleManager.RoleExists("Authors"))
+            {
+                role = new IdentityRole();
+                role.Name = "Authors";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Readers"))
+            {
+                role = new IdentityRole();
+                role.Name = "Readers";
+                roleManager.Create(role);
+            }
+        }
+        public void CreateUser()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var user = new ApplicationUser();
+            user.Email = "Mohamed@gmail.com";
+            user.UserName = "Mohamed";
+            var check = userManager.Create(user, "ASDasd@123");
+            if (check.Succeeded)
+            {
+                userManager.AddToRole(user.Id, "Admins");
+            }
+        }
+       
+    }
+}
